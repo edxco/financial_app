4369ef61d8a1e62af84521def2151a5795d5c55c
edxco <eduardo.baeza@yahoo.com>
2021-01-09 14:34:25 -0600

    First push to heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,3 @@
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 4, message: 'Name is too short...' }
-    mount_uploader :avatar, AvatarUploader
-    validates_presence_of :avatar
 end
