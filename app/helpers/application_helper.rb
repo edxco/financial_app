9f0248bb37d314514569eb74ef315677861fed8b
edxco <eduardo.baeza@yahoo.com>
2021-01-11 07:20:04 -0600

    Avatar feature testing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,6 +16,14 @@ module ApplicationHelper
     end
   end
 
+  def check_logged
+    if logged_in?
+      concat link_to "Logout", logout_path, method: :delete, class: "lg:p-4 py-3 px-0 font-bold block border-b-2 border-transparent hover:border-indigo-400 lg:mb-0 mb-2"
+    else
+      concat link_to "Login", login_path, class: "lg:p-4 py-3 px-0 font-bold block border-b-2 border-transparent hover:border-indigo-400 lg:mb-0 mb-2"
+    end
+  end
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
 end
